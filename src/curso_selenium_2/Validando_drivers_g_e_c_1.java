3734fac2ec7a617be941155d3258e334528140b1
gerjimenez <gerjimenez@gmail.com>
2025-03-01 16:42:31 +0100

    Update  con nuevos metodos
    Añadidos metodos basicos

--- src/curso_selenium_2/Validando_drivers_g_e_c_1.java
+++ src/curso_selenium_2/Validando_drivers_g_e_c_1.java
@@ -8,11 +8,19 @@ import org.openqa.selenium.edge.EdgeDriver;
 public class Validando_drivers_g_e_c_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		   //validando edgedriver
 			  System.setProperty("webdriver.edge.driver", "C:\\Users\\Gerardo Jimenez Diez\\drivers_webs\\edgedriver\\msedgedriver.exe");
 			  WebDriver driver_edge = new EdgeDriver();
 			  driver_edge.get("https://www.google.com");
+			  // metodo basico 2 getTitle = nos trae el titulo de las pagina actual
+			  System.out.println(driver_edge.getTitle());
+			  // metodo basico 3, getCurrentUrl() muestra la direcdcion actual de la pagina
+			  System.out.println(driver_edge.getCurrentUrl());
+			  // metodo basico 4, close() cierra el navegador
+			  // driver_edge.close();
+			  // metodo basico 5, quit() termina la sesion del driver
+			  driver_edge.quit();;
+			  
 			  
 		// validando chrome driver
 		// System.setProperty("webdriver.chrome.driver", "C:\\Users\\Gerardo Jimenez Diez\\drivers_webs\\chromedriver\\chromedriver.exe");
